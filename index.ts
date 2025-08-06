330c7e4cc9d2f71e2332faae84c6c03af4203e82
Trần Minh Huy <justhuytran@MacBook-Air-cua-Tran.local>
2025-08-06 20:05:00 +0700

    Cài đặt cơ bản

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
   import express, { Application, Request, Response } from 'express';
   import dotenv from 'dotenv';
+  import path from 'path';
+
 
   dotenv.config();
 
   const app: Application = express();
   const port = process.env.PORT;
 
-  app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from Express and TypeScript!');
+  //pug
+  app.set('views', path.join(__dirname, 'views')); // thêm path.join
+  app.set('view engine', 'pug')
+
+  app.get('/tours', (req: Request, res: Response) => {
+    res.render("client/pages/tours/index")
   });
 
   app.listen(port, () => {
