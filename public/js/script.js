af3b0c20d201ea3ff5d8517e5622c72021a1280a
Trần Minh Huy <justhuytran@MacBook-Air-cua-Tran.local>
2025-08-17 09:09:36 +0700

    thêm tour vào giỏ hàng localstorage bên front-end

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,40 @@ var swiper = new Swiper(".mySwiper", {
     });
 
 // ảnh động slider của swiper 
+
+
+// cart 
+const cart=localStorage.getItem("cart")
+  // nếu chưa có giỏ hàng thì tạo giỏ hàng mới
+if(!cart){
+ localStorage.setItem("cart",JSON.stringify([]))//trong localStorgae luôn đưa chuỗi
+}
+
+  // thêm tour vào giỏ hàng 
+  const formAddToCart=document.querySelector("[form-add-to-cart]")
+  if(formAddToCart){
+    formAddToCart.addEventListener("submit",(e)=>{
+      e.preventDefault()
+      const quantity=parseInt(e.target.elements.quantity.value)
+      const tourId=parseInt(formAddToCart.getAttribute("tour-id"))
+
+      if(quantity>0 && tourId ){
+        const cart=JSON.parse(localStorage.getItem("cart")) //trả vể mảng dạng json nên phải chuyển đổi
+
+        //kiểm tra có tour dã dc đăng ký trước đó chưa để cộng vào còn ko có thì thêm mới
+        const indexExistTour=cart.findIndex(item=>item.tourId==tourId)
+        if(indexExistTour==-1){
+          //thêm mới nếu ko có
+          cart.push({
+            tourId:tourId,
+            quantity:quantity
+          })
+        }else{
+          cart[indexExistTour].quantity=cart[indexExistTour].quantity+quantity
+        }
+        
+        localStorage.setItem("cart",JSON.stringify(cart)) //update lại do cart mới push là mảng mới và phải chuyển đổi JSON
+      }
+    })
+  }
+// end cart 
